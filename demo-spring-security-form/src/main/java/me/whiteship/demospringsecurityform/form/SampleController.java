365fa0fe95f3405622b552ce9c8139fbbffc86a7
USER <user@AL01414032.local>
2020-08-22 22:28:04 +0900

    init

---
+++ demo-spring-security-form/src/main/java/me/whiteship/demospringsecurityform/form/SampleController.java
@@ -0,0 +1,40 @@
+package me.whiteship.demospringsecurityform.form;
+
+import java.security.Principal;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SampleController {
+	@GetMapping("/")
+	public String index(Model model,Principal principal) {
+		if(principal==null) {
+			model.addAttribute("message", "Hello Spring Security");
+		}else {
+			model.addAttribute("message", "Hello" + principal.getName());
+			
+		}
+		return "index";
+	}
+
+	@GetMapping("/info")
+	public String info(Model model) {
+		model.addAttribute("message", "info");
+		return "info";
+	}
+
+	@GetMapping("/admin")
+	public String admin(Model model, Principal principal) {
+		model.addAttribute("message", "Hello"+principal.getName());
+		return "admin";
+	}
+
+	@GetMapping("/dashboard")
+	public String dashboard(Model model, Principal principal) {
+		model.addAttribute("message", "Hello Admin"+principal.getName());
+		return "dashboard";
+	}
+
+}
